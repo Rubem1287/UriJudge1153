ce3791e8e7d9d8a1762eea44776df3c58bceb69f
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-16 16:34:19 -0300

    Add files via upload

---
+++ Uri1153/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Uri1153
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, fatorial, i,c;
+            i = 0;
+            c = 0;
+            int[] vetor;
+
+            vetor = new int[13];
+
+            fatorial = 0;
+
+            n = int.Parse(Console.ReadLine());
+
+            while (n > 13)
+            {
+                n = int.Parse(Console.ReadLine());
+            }
+
+            for (c = n; c > 0; c--)
+            {
+                if (i == 0)
+                {
+                    i = 1;
+                    //vetor[c] = c;
+                    fatorial = c;
+                }
+                else
+                {
+                    //vetor[c] = c;
+                    fatorial = fatorial * c;                }
+            }
+
+            Console.WriteLine(fatorial);
+        }
+    }
+}
